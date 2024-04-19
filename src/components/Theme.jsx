78d421e99727bb8faafdd96e37c3804441f40431
mlaflam <mlaflam@users.noreply.github.com>
2024-04-19 14:02:43 -0400

    Editing theme and display with MaterialUI

---
+++ src/components/Theme.jsx
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles';
+
+const Theme = createTheme({
+  palette: {
+    primary: {
+      main: '#d4d4d4', // Replace with your custom primary color
+    }
+  },
+});
+
+export default Theme;
